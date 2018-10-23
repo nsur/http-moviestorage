14523fcbd7d6baa255146eb25274f0d5ae0a71d6
nsur <surgovan@gmail.com>
2018-10-23 09:06:08 +0300

    Update configuration

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
  * Time: 16:40
  */
 define('ROOT', dirname(__FILE__));
+define('DATABASE_INSTALLED', true);
+
+// Local Site
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASSWORD', '');
 define('DATABASE', 'moviestorage');
-define('DATABASE_INSTALLED', true);
+
+// Hosting http://moviestor-cc-ua.1gb.ua/
+//define('HOST', '');
+//define('USER', '');
+//define('PASSWORD', '');
+//define('DATABASE', '');
 
 function __autoload($className) {
 	$ds = DIRECTORY_SEPARATOR;
